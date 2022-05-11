04e05e10f71f5c2b70627397bc7616e9dff09eed
Ingrid_Ramos <ingridrcs@gmail.com>
2022-05-10 22:46:59 -0300

    requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,6 @@
+SELECT H.historico_de_reproducoes AS nome, COUNT(U.usuario_id) AS 'reproducoes'
+FROM SpotifyClone.historicos AS H
+INNER JOIN SpotifyClone.usuarios AS U
+ON H.usuario_id = U.usuario_id
+WHERE planos_id = 1 OR planos_id = 4
+GROUP BY nome;
--- desafio7.sql
+++ desafio7.sql
@@ -6,3 +6,5 @@ INNER JOIN SpotifyClone.usuario_seguindo_artistas AS US
 ON AL.artista_id = US.artista_id
 GROUP BY AR.artista, AL.album
 ORDER BY seguidores DESC, artista ASC, AL.album ASC;
+
+--Source: https://www.devmedia.com.br/sql-order-by/41225#:~:text=SQL%3A%20Order%20by-,ORDER%20BY%20organiza%20os%20resultados%20de%20acordo%20com%20uma%20ou,(ASC)%2C%20por%20padr%C3%A3o. -- 
