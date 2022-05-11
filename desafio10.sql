6a69b96019f84d8160b81106692ef36d4995e611
Ingrid_Ramos <ingridrcs@gmail.com>
2022-05-10 22:52:04 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -3,4 +3,5 @@ FROM SpotifyClone.historicos AS H
 INNER JOIN SpotifyClone.usuarios AS U
 ON H.usuario_id = U.usuario_id
 WHERE planos_id = 1 OR planos_id = 4
-GROUP BY nome;
+GROUP BY nome
+ORDER BY nome;
